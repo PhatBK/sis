fa23ece51578cfc5e34fe083b546c5d17fb562d5
Nguyễn Huy Phát <phatSN11081996@gmail.com>
2018-11-17 16:43:40 +0700

    Update RegistSubject.php

--- sis/app/Models/RegistSubject.php
+++ sis/app/Models/RegistSubject.php
@@ -10,4 +10,21 @@ class RegistSubject extends Model
     public function getAllRegistSubject() {
     	return DB::table('regist_subjects')->get();
     }
+    public function createRegistSubject($arrays) {
+    	$success = false;
+    	if(count($arrays) != 0) {
+	    	DB::table('regist_subjects')->insert([
+	    		[
+	    			'code_student' => $arrays['code_student'],
+	    			'code_subject' => $arrays['code_subject'],
+                    'created_at' => $arrays['date'],
+	    		],
+	    	]);
+	    	$success = true;
+    	}
+    	return $success;
+    }
+    public function getLastRecord() {
+        
+    }
 }
